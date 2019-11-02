c4da274eefe2e61a31bdfe484ffd051e27f048bf
Daniel Stieglmayr <daniel.stieglmayr@rcapp.de>
2019-11-02 21:30:15 +0100

    Fixes

--- scripts/RSA.js
+++ scripts/RSA.js
@@ -1,6 +1,6 @@
 function primzahl(n) {
-    var arr = [2];
-    for (var i = 3; i < n; i += 2) {
+    let arr = [2];
+    for (let i = 3; i < n; i += 2) {
         if (isPrimeNumber(i)) {
             arr.push(i);
         }
@@ -9,7 +9,7 @@ function primzahl(n) {
 }
 
 function isPrimeNumber(num) {
-    for (var i = 2; i < num; i++) {
+    for (let i = 2; i < num; i++) {
         if (num % i === 0) {
             return false;
         }
@@ -28,7 +28,7 @@ function calcPhiN(p, q) {
 
 function primeToPhi(primeArray, phi) {
 
-    var randomNum = (Math.random() * Math.floor(primeArray.length));
+    let randomNum = (Math.random() * Math.floor(primeArray.length));
     randomNum = Math.round(randomNum);
     if (phi != primeArray[randomNum] && primeArray[randomNum] < phi) {
         return primeArray[randomNum];
@@ -41,8 +41,8 @@ function primeToPhi(primeArray, phi) {
 
 function calcD(e, phiN) {
     /* d = (1 + k * fi)/e */
-    var d;
-    var k = 1;
+    let d;
+    let k = 1;
     while (true) {
         d = (1 + k * phiN) / e;
         if (d == parseInt(d)) {
@@ -63,14 +63,14 @@ function pick2Numbers(max) {
 }
 
 function RSAAlgo() {
-    var primeArray = primzahl(100);
-    var randomNum = pick2Numbers(primeArray.length);
-    var p = primeArray[randomNum[0]];
-    var q = primeArray[randomNum[1]];
-    var n = calculateN(p, q);
-    var phiN = (p - 1) * (q - 1);
-    var e = primeToPhi(primeArray, phiN);
-    var d = calcD(e, phiN);
+    let primeArray = primzahl(100);
+    let randomNum = pick2Numbers(primeArray.length);
+    let p = primeArray[randomNum[0]];
+    let q = primeArray[randomNum[1]];
+    let n = calculateN(p, q);
+    let phiN = calcPhiN(p,q);
+    let e = primeToPhi(primeArray, phiN);
+    let d = calcD(e, phiN);
 
     /*  console.log(p);
      console.log(q);
@@ -86,7 +86,7 @@ function RSAAlgo() {
 }
 
 function KeyGen() {
-    var values;
+    let values;
     while (true) {
         values = RSAAlgo();
         if (values[5] != -1) {
@@ -106,16 +106,16 @@ function KeyGen() {
 
 function encrypt() {
     if ($('#text').val()) {
-        var N = $('#componentN').val();
+        let N = $('#componentN').val();
         N = parseInt(N);
-        var e = $('#componente').val();
+        let e = $('#componente').val();
         e = parseInt(e);
-        var text = $('#text').val();
-        var Output = "";
-        for (var i in text) {
-            var KeyCode = text.charCodeAt(i);
+        let text = $('#text').val();
+        let Output = "";
+        for (let i in text) {
+            let KeyCode = text.charCodeAt(i);
 
-            KeyCode = powFun(KeyCode, e, N)
+            KeyCode = powFun(KeyCode, e, N);
             Output += KeyCode;
             Output += " ";
         }
@@ -132,14 +132,14 @@ function addmod(x, y, n) {
 }
 
 function sqrmod(a, n) {
-    var b;
-    var sum = 0;
+    let b;
+    let sum = 0;
 
     // Make sure original number is less than n
     a = a % n;
 
     // Use double and add algorithm to calculate a*a mod n
-    for (b = a; b != 0; b >>= 1) {
+    for (b = a; b !== 0; b >>= 1) {
         if (b & 1) {
             sum = addmod(sum, a, n);
         }
@@ -149,7 +149,7 @@ function sqrmod(a, n) {
 }
 
 function powFun(base, ex, mo) {
-    var r;
+    let r;
     if (ex === 0)
         return 1;
     else if (ex % 2 === 0) {
